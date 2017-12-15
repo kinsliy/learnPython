5ffd1ce38604c64540110454f48c2c461137930c
刘威 <liuwei@192.168.0.101>
2017-12-15 21:18:41 +0800

    list

--- demo1/demo.py
+++ demo1/demo.py
@@ -13,4 +13,23 @@ love ='i love you ';
 print (love[2:6]);
 
 
+list1 =['hello',''];
+
+list1[1]='world';
+
+print (list1);
+
+list2 =['hello']
+
+for x in ['','','','']:
+    print (list2);
+
+print (len(list2));
+print(list2*3);
+
+list3=('hello','wolrd');
+
+print(list3[1])
+
+
 
