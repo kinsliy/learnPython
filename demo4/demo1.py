bf493db5a4964b2d60f89c672dd1131bf9b6425f
kinsliy <kinsliy@qq.com>
2017-12-21 21:08:42 +0800

    index

--- demo4/demo1.py
+++ demo4/demo1.py
@@ -1,5 +1,37 @@
 
 
 
-#二分法 算法实现
+#二分法 算法实现 二分法需要是有序的集合，这里使用数组
+
+
+def search(list,item):
+    low=0;
+    high=len(list)-1;
+
+    while low <=high:
+        mid=int((low+high)/2);
+        guess=list[mid];
+
+        if guess==item:
+            print(mid)
+            return 
+        elif guess>item:
+            high=mid-1
+        else:
+            low=mid+1
+    print('not this number');
+
+myList=[1,3,5,7,9];
+
+search(myList,3);
+search(myList,-1);
+search(myList,9)
+
+
+
+
+
+
+
+
 
